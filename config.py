bf97d25742302c693fa64701e722083875d09649
Matt Creel <mcreel31@gmail.com>
2019-12-23 21:24:30 +0000

    certbot command update

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config(object):
-    API_KEY = '79c943bef0de85d447b41afe05591f7f2ccb2bc9994074f8'
+    API_KEY = '82dcba2e725646f06b0e1a905e33205fbc584b22846e75c7'
     CERT_PATH = 'redlure-cert.pem'
     KEY_PATH = 'redlure-key.pem'
     WORKER_PORT = 4445
---
+++ install.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y gunicorn3 gcc python3-pip python3-dev
+pip3 install pipenv
+pip3 install flask
+pip3 install requests
+pip3 install psutil
+pipenv install
