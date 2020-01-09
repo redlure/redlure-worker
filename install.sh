273b843cf7bb707240626f05010a0c29170cef13
Matt Creel <37981031+Tw1sm@users.noreply.github.com>
2020-01-09 11:56:35 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y gunicorn gcc python3.7 python3-pip python3-dev python3.7-dev
+sudo apt-get install -y gunicorn gcc python3.7 python3-pip python3-dev python3.7-dev certbot
 pip3 install pipenv
 pip3 install flask
 pip3 install requests
