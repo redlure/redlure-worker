d99035b23a746058677f1d543fdfeb253ead7bda
Tw1sm <mcreel31@gmail.com>
2022-01-08 23:03:11 +0000

    bump to python3.8

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
-sudo apt-get install -y python3-pip gunicorn gcc python3.7 python3.7-dev certbot
+sudo apt-get install -y python3-pip gunicorn gcc certbot
 pip3 install pipenv
 pip3 install flask
 pip3 install requests
