4ec789f504e9593bc394d950b2a1de87a35dd63d
Matt Creel <37981031+Tw1sm@users.noreply.github.com>
2022-01-21 15:18:34 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 class Config(object):
-    API_KEY = ''
+    API_KEY = '' # Get the value from the 'Domains & Servers' page of your redlure-console
     CERT_PATH = 'redlure-cert.pem'
     KEY_PATH = 'redlure-key.pem'
     WORKER_PORT = 4445
-    SERVER_IP = ''
+    SERVER_IP = '' # IP or domain name of your redlure-console
     SERVER_PORT = 5000
     UPLOAD_FOLDER = '/tmp/redlure/uploads'
